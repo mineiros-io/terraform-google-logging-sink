2f29a2a6a51bb9695f8b8ad996580620f6838b28
Marius Tolzmann <marius@mineiros.io>
2022-03-21 12:44:03 +0100

    chore: initial adjustments

--- examples/example/main.tf
+++ examples/example/main.tf
@@ -4,8 +4,8 @@
 # And some more meaningful information.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-module "terraform-module-template" {
-  source = "git@github.com:mineiros-io/terraform-module-template.git?ref=v0.0.1"
+module "terraform-google-logging-sink" {
+  source = "git@github.com:mineiros-io/terraform-google-logging-sink.git?ref=v0.0.1"
 
   # All required module arguments
 
