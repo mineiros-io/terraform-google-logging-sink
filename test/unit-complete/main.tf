830744354f2a857c19496d1f7c5b65f118641664
Nathan Parraga Thiesen <nathan.thiesen@mineiros.io>
2022-03-22 11:41:52 +0100

    chore: set up tests

--- test/unit-complete/main.tf
+++ test/unit-complete/main.tf
@@ -4,23 +4,13 @@
 # The purpose is to activate everything the module offers, but trying to keep execution time and costs minimal.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-variable "aws_region" {
-  description = "(Optional) The AWS region in which all resources will be created."
-  type        = string
-  default     = "us-east-1"
-}
-
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+    google = {
+      source  = "hashicorp/google"
+      version = "4.1.12"
     }
   }
-}
-
-provider "aws" {
-  region = var.aws_region
 }
 
 # DO NOT RENAME MODULE NAME
@@ -30,14 +20,24 @@ module "test" {
   module_enabled = true
 
   # add all required arguments
+  name        = "my-pubsub-instance-sink"
+  destination = "pubsub.googleapis.com/projects/my-project/topics/instance-activity"
 
   # add all optional arguments that create additional resources
-
-  # add most/all other optional arguments
-
-  # module_tags = {
-  #   Environment = "unknown"
-  # }
+  description            = "some explanation on what this is"
+  filter                 = "resource.type = gce_instance"
+  unique_writer_identity = true
+  use_partitioned_tables = true
+  exclusions = [{
+    name        = "nsexcllusion1"
+    description = "Exclude logs from namespace-1 in k8s"
+    filter      = "resource.type = k8s_container resource.labels.namespace_name=\"namespace-1\" "
+    },
+    {
+      name        = "nsexcllusion2"
+      description = "Exclude logs from namespace-2 in k8s"
+      filter      = "resource.type = k8s_container resource.labels.namespace_name=\"namespace-2\" "
+  }]
 
   module_depends_on = ["nothing"]
 }
