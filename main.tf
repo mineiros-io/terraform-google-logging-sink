45a8ddce59da2b91ef2ca2fba6f63984d9403a77
Nathan Parraga Thiesen <nathan.thiesen@mineiros.io>
2022-03-22 11:33:34 +0100

    feat: initial module implementation

--- main.tf
+++ main.tf
@@ -3,3 +3,34 @@
 # And it continues with some lowercase information about the module
 # We might add more than one line for additional information
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+resource "google_logging_project_sink" "sink" {
+  count = var.module_enabled ? 1 : 0
+
+  name        = var.name
+  destination = var.destination
+
+  filter                 = var.filter
+  description            = var.description
+  disabled               = var.disabled
+  project                = var.project
+  unique_writer_identity = var.unique_writer_identity
+
+  # TODO: is this correct?
+  bigquery_options {
+    use_partitioned_tables = var.use_partitioned_tables
+  }
+
+  dynamic "exclusions" {
+    for_each = var.exclusions
+    iterator = exclusion
+
+    content {
+      name        = exclusion.value.name
+      filter      = exclusion.value.filter
+      description = try(exclusion.value.description, null)
+    }
+  }
+
+  depends_on = [var.module_depends_on]
+}
--- variables.tf
+++ variables.tf
@@ -3,10 +3,104 @@
 # These variables must be set when using this module.
 # ----------------------------------------------------------------------------------------------------------------------
 
+variable "name" {
+  type        = string
+  description = "(Required) The name of the logging sink."
+}
+
+variable "destination" {
+  type        = string
+  description = <<-END
+    (Required) The destination of the sink (or, in other words, where logs are written to).
+
+    Can be a Cloud Storage bucket, a PubSub topic, a BigQuery dataset or a Cloud Logging bucket.
+
+    Examples:
+    - `"storage.googleapis.com/[GCS_BUCKET]"`
+    - `"bigquery.googleapis.com/projects/[PROJECT_ID]/datasets/[DATASET]"`
+    - `"pubsub.googleapis.com/projects/[PROJECT_ID]/topics/[TOPIC_ID]"`
+    - `"logging.googleapis.com/projects/[PROJECT_ID]]/locations/global/buckets/[BUCKET_ID]"`
+
+    The writer associated with the sink must have access to write to the above resource.
+  END
+}
+
 # ----------------------------------------------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 # These variables have defaults, but may be overridden.
 # ----------------------------------------------------------------------------------------------------------------------
+
+variable "filter" {
+  type        = string
+  description = "(Optional) The filter to apply when exporting logs. Only log entries that match the filter are exported."
+  default     = null
+}
+
+variable "description" {
+  type        = string
+  description = "(Optional) A description of this sink. The maximum length of the description is 8000 characters."
+  default     = null
+}
+
+variable "disabled" {
+  type        = bool
+  description = "(Optional) If set to True, then this sink is disabled and it does not export any log entries."
+  default     = null
+}
+
+variable "project" {
+  type        = string
+  description = "(Optional) The ID of the project to create the sink in. If omitted, the project associated with the provider is used."
+  default     = null
+}
+
+variable "unique_writer_identity" {
+  type        = bool
+  description = <<-END
+    (Optional) Whether or not to create a unique identity associated with this sink.
+
+    If `false` (the default), then the `writer_identity` used is `serviceAccount:cloud-logs@system.gserviceaccount.com`.
+
+    If `true`, then a unique service account is created and used for this sink. If you wish to publish logs across projects or utilize `bigquery_options`, you must set `unique_writer_identity` to true.
+  END
+  default     = null
+}
+
+# TODO: is this a good way for dealing with `bigquery_options` attribute?
+variable "use_partitioned_tables" {
+  type        = bool
+  description = <<-END
+    Whether to use BigQuery's partition tables.
+
+    By default, Logging creates dated tables based on the log entries' timestamps, e.g. syslog_20170523. With partitioned tables the date suffix is no longer present and special query syntax has to be used instead. In both cases, tables are sharded based on UTC timezone.
+  END
+  default     = false
+}
+
+# variable "bigquery_options" {
+#   type = object({
+#     use_partitioned_tables = bool
+#   }) # map(string)? any?
+#   description = "(Optional) Options that affect sinks exporting data to BigQuery."
+#   default     = null
+# }
+
+variable "exclusions" {
+  # type = list(object({
+  #   name        = string
+  #   filter      = string
+  #   description = optional(string)
+  # }))
+  type = any # TODO: is this correct?
+
+  description = <<-END
+    Log entries that match any of the exclusion filters will not be exported.
+
+    If a log entry is matched by both filter and one of `exclusion_filters` it will not be exported. Can be repeated multiple times for multiple exclusions.
+  END
+  default     = []
+}
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 # MODULE CONFIGURATION PARAMETERS
@@ -18,18 +112,6 @@ variable "module_enabled" {
   description = "(Optional) Whether or not to create resources within the module."
   default     = true
 }
-
-# variable "module_tags" {
-#   type        = map(string)
-#   description = "(Optional) A map of tags that will be applied to all created resources that accept tags. Tags defined with 'module_tags' can be overwritten by resource-specific tags."
-#   default     = {}
-# }
-
-# variable "module_timeouts" {
-#   description = "(Optional) A map of timeout objects that is keyed by Terraform resource name defining timeouts for `create`, `update` and `delete` Terraform operations."
-#   type        = any
-#   default     = null
-# }
 
 variable "module_depends_on" {
   type        = any
